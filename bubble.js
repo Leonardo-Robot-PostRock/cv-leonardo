f050811bd51aac53006f6b2cae05f01f9db13145
Leonardo <leonardo38361@gmail.com>
2024-03-12 05:35:04 -0300

    effecto de burbujas

---
+++ bubble.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('.header');
+  const numBubbles = 80;
+
+  // Función para crear una burbuja
+  function createBubble() {
+    const bubble = document.createElement('span');
+    bubble.classList.add('bubble');
+    bubble.style.width = Math.random() * 20 + 'px';
+    bubble.style.height = bubble.style.width;
+    bubble.style.left = Math.random() * 100 + '%';
+    bubble.style.animationDuration = Math.random() * 6 + 5 + 's';
+    bubble.style.opacity = Math.random();
+    bubble.style.bottom = -20 + 'px';
+    bubble.style.zIndex = '1';
+    bubble.style.backgroundColor = 'rgba(173, 216, 230, 0.8)'; // Color cian claro
+    header.appendChild(bubble);
+
+    // Elimina la burbuja después de la animación
+    bubble.addEventListener('animationend', function () {
+      bubble.remove();
+    });
+  }
+
+  // Crea múltiples burbujas
+  for (let i = 0; i < numBubbles; i++) {
+    setTimeout(createBubble, Math.random() * 2000);
+  }
+});
